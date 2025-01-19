2ce25fb5d505d63fa9d928fad17c8dfed726535f
Mathias <mathias.pachecolemina@mail.mcgill.ca>
2025-01-19 00:01:40 -0500

    Login - Trying to get the API to work

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -3,10 +3,27 @@ import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './auth.component.html',
-  styleUrl: '../app.component.css'
+  styleUrls: ['../app.component.css'] // Corrected from styleUrl to styleUrls
 })
 export class AuthComponent {
+  // Add your new method here
+  ngOnInit() {
+    document.getElementById('loginButton')?.addEventListener('click', this.onLogin.bind(this));
+  }
 
+  onLogin() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://reqres.in/api/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    const data = JSON.stringify({
+      email: "eve.holt@reqres.in",
+      password: "cityslicka"
+    });
+    xhr.send(data);
+    xhr.onload = function() {
+      alert(xhr.responseText);
+    }
+  }
 }
