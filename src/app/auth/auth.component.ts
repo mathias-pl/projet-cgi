573d318cbcb60a8ca5948fc76189251dfef94a18
Mathias <mathias.pachecolemina@mail.mcgill.ca>
2025-01-19 00:32:24 -0500

    Login MVP

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,25 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterLinkActive, RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLinkActive, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, FormsModule],
   templateUrl: './auth.component.html',
   styleUrls: ['../app.component.css'] // Corrected from styleUrl to styleUrls
 })
 export class AuthComponent {
-  // Add your new method here
-  ngOnInit() {
-    document.getElementById('loginButton')?.addEventListener('click', this.onLogin.bind(this));
-  }
+  email: string = '';
+  password: string = '';
 
-  onLogin() {
+  onLogin(email: string, password: string): void {
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://reqres.in/api/login', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     const data = JSON.stringify({
-      email: "eve.holt@reqres.in",
-      password: "cityslicka"
+      email: email,
+      password: password
     });
     xhr.send(data);
     xhr.onload = function() {
