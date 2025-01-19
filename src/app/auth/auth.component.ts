effe24d29672b079c60e7132d06e4b36041dae3b
Mathias <mathias.pachecolemina@mail.mcgill.ca>
2025-01-19 01:00:27 -0500

    Adding logout button
    For login, the password should not be checked. Also removed the navbar

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLinkActive, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { every } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
@@ -12,7 +13,28 @@ export class AuthComponent {
   email: string = '';
   password: string = '';
 
+  validateEmail(email: string): boolean {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+  }
+
+  validatePassword(password: string): boolean {
+    const hasLength = password.length >= 8;
+    const hasUpperCase = /[A-Z]/.test(password);
+    const hasLowerCase = /[a-z]/.test(password);
+    const hasNumber = /\d/.test(password);
+    return hasLength && hasUpperCase && hasLowerCase && hasNumber;
+  }
+
   onLogin(email: string, password: string): void {
+    if (!this.validateEmail(email)) {
+      alert('Please enter a valid email address.');
+      return;
+    }
+    // if (!this.validatePassword(password)) {
+    //   alert('Please enter a valid password.');
+    //   return;
+    // }
     var xhr = new XMLHttpRequest();
     xhr.open('POST', 'https://reqres.in/api/login', true);
     xhr.setRequestHeader('Content-Type', 'application/json');
@@ -22,7 +44,13 @@ export class AuthComponent {
     });
     xhr.send(data);
     xhr.onload = function() {
-      alert(xhr.responseText);
+      if (xhr.status === 200) {
+        const response = JSON.parse(xhr.responseText);
+        localStorage.setItem('token', response.token);
+        window.location.href = '/gestion';
+      } else {
+        alert('Login failed. Please check your credentials and try again.');
+      }
     }
   }
 }
--- src/app/gestion/gestion.component.ts
+++ src/app/gestion/gestion.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-gestion',
   imports: [],
   templateUrl: './gestion.component.html',
-  styleUrl: './gestion.component.css'
+  styleUrl: '../app.component.css'
 })
-export class GestionComponent {
 
+export class GestionComponent implements OnInit {
+  token: string | null = '';
+
+  ngOnInit(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  onLogout(): void {
+    localStorage.removeItem('token');
+    window.location.href = '/auth';
+  }
 }
