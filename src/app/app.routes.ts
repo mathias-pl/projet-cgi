f41a597639261338a29bfc81240613f17aba53e0
Mathias <mathias.pachecolemina@mail.mcgill.ca>
2025-01-18 19:10:59 -0500

    Init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { DetailsComponent } from './details/details.component';
+import { GestionComponent } from './gestion/gestion.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'auth', component: AuthComponent },
+    { path: 'inscription', component: InscriptionComponent },
+    { path: 'details', component: DetailsComponent },
+    { path: 'gestion', component: GestionComponent },
+    { path: '**', component: PageNotFoundComponent }
+];
